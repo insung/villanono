93f13aa75796589593848a750e022d6deba19beb
insung <imissyoubrad@gmail.com>
2024-10-05 13:32:30 +0900

    sub chart 만 남기고 나머지 차트는 다 삭제

--- 📈인사이트.py
+++ 📈인사이트.py
@@ -1,5 +1,6 @@
+import datetime
+
 import pandas as pd
-import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
 from plotly.subplots import make_subplots
@@ -54,126 +55,68 @@ choices = [
 
 size_choices = ["전체", "소형(60㎡미만)", "중형(80㎡미만)", "대형(80㎡이상)"]
 
-with col1:
+today = datetime.datetime.today()
+three_months_ago = today - datetime.timedelta(days=90)
+
+with col4:
     selected_size = st.selectbox(
         label="면적:", options=size_choices, index=size_choices.index("전체")
-    )
-
-with col3:
-    selected = st.selectbox(
-        label="지표:", options=choices, index=choices.index(default_value)
     )
 
 df = get_dataframe_for_insight(begin_year, end_year, si, gu, dong, selected_size)
 
 df["계약년월"] = pd.to_datetime(df["계약년월"], format="%Y%m")
-tickvals = df["계약년월"]
-ticktext = df["계약년월"].dt.strftime("%Y-%m")
 
-if selected == "종합":
-    fig = px.line(df, x="계약년월", y="평균(만원)", markers=True)
-    fig.update_xaxes(type="category", tickvals=tickvals, ticktext=ticktext)
-    fig.update_yaxes(tickformat=",.0f")
-    fig.add_scatter(
+fig = make_subplots(
+    rows=2,
+    cols=1,
+    shared_xaxes=True,
+    vertical_spacing=0.1,
+    row_heights=[0.8, 0.2],
+    subplot_titles=("가격(만원)", "거래량(건)"),
+)
+
+fig.add_trace(
+    go.Scatter(
         x=df["계약년월"],
-        y=df["최소(만원)"],
-        mode="markers",
-        name="최소",
-        marker=dict(color="#E8E8E8", size=5),
-        hoverinfo="none",
-        showlegend=False,
-    )
-    fig.add_scatter(
+        y=df["평균(만원)"],
+        hovertemplate="%{x|%Y-%m} - %{y}만원",
+        name="",
+    ),
+    row=1,
+    col=1,
+)
+fig.add_trace(
+    go.Bar(
         x=df["계약년월"],
-        y=df["최대(만원)"],
-        mode="markers",
-        name="최대",
-        marker=dict(color="#E8E8E8", size=5),
-        hoverinfo="none",
-        showlegend=False,
-    )
-
-    fig.update_traces(
-        hovertemplate="<b>%{x}</b><br /><br />평균(만원): %{customdata[0]:,.0f}<br />최소(만원): %{customdata[1]:,.0f}<br />최대(만원): %{customdata[2]:,.0f}",
-        customdata=df[["평균(만원)", "최소(만원)", "최대(만원)"]].values,
-    )
-    st.plotly_chart(fig)
-elif selected == "거래량(건)":
-    fig = make_subplots(
-        rows=2,
-        cols=1,
-        shared_xaxes=True,
-        vertical_spacing=0.1,
-        row_heights=[0.8, 0.2],
-        subplot_titles=("가격(만원)", "거래량(건)"),
-    )
-
-    fig.add_trace(
-        go.Scatter(
-            x=df["계약년월"],
-            y=df["평균(만원)"],
-            hovertemplate="%{x|%Y-%m} - %{y}만원",
-            name="",
+        y=df["거래량(건)"],
+        hovertemplate="%{x|%Y-%m} - %{y}건",
+        name="",
+    ),
+    row=2,
+    col=1,
+)
+fig.update_yaxes(tickformat=",.0f")
+fig.update_layout(
+    height=600,
+    showlegend=False,
+    xaxis=dict(
+        rangeselector=dict(
+            buttons=list(
+                [
+                    dict(count=1, label="1년", step="year", stepmode="backward"),
+                    dict(count=3, label="3년", step="year", stepmode="backward"),
+                    dict(count=10, label="10년", step="year", stepmode="backward"),
+                    dict(step="all", label="전체", visible=False),
+                ]
+            )
         ),
-        row=1,
-        col=1,
-    )
-    fig.add_trace(
-        go.Bar(
-            x=df["계약년월"],
-            y=df["거래량(건)"],
-            hovertemplate="%{x|%Y-%m} - %{y}건",
-            name="",
-        ),
-        row=2,
-        col=1,
-    )
-    fig.update_yaxes(tickformat=",.0f")
-    fig.update_layout(
-        height=600,
-        showlegend=False,
-        xaxis=dict(
-            rangeselector=dict(
-                buttons=list(
-                    [
-                        dict(count=3, label="3년", step="year", stepmode="backward"),
-                        dict(count=10, label="10년", step="year", stepmode="backward"),
-                        dict(step="전체"),
-                    ]
-                )
-            ),
-            rangeslider=dict(visible=False),
-            type="date",
-        ),
-    )
-    st.plotly_chart(fig, use_container_width=True)
-else:
-    fig = px.line(df, x="계약년월", y=selected)
-    fig.update_xaxes(type="category", tickvals=tickvals, ticktext=ticktext)
-    fig.update_yaxes(tickformat=",.0f")
-
-    fig.update_layout(
-        xaxis=dict(
-            rangeselector=dict(
-                buttons=list(
-                    [
-                        dict(count=1, label="1년", step="year", stepmode="backward"),
-                        dict(count=3, label="3년", step="year", stepmode="backward"),
-                        dict(count=10, label="10년", step="year", stepmode="backward"),
-                        dict(step="all", label="전체", visible=False),
-                    ]
-                )
-            ),
-            rangeslider=dict(visible=False),
-            type="date",
-        )
-    )
-
-    st.plotly_chart(fig)
-
-
-# df["계약년월"] = df["계약년월"].dt.strftime("%Y-%m")
-# st.dataframe(df, hide_index=True)
-
-# st.write(df["계약년월"].min(), df["계약년월"].max())
-# print(type(df["계약년월"].min()))
+        rangeslider=dict(visible=False),
+        type="date",
+        range=[
+            three_months_ago.strftime("%Y-%m-%d"),
+            today.strftime("%Y-%m-%d"),
+        ],  # 3년 범위 설정
+    ),
+)
+st.plotly_chart(fig, use_container_width=True)
