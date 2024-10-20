292f4d7b64ab9f27cacd814767464c2a9a0989de
insung <imissyoubrad@gmail.com>
2024-10-20 19:20:35 +0900

    about 페이지 추가

---
+++ pages/9_💬About.py
@@ -0,0 +1,39 @@
+import streamlit as st
+
+from page_config import add_page_config
+from sidebar import add_sidebar
+
+#### page config ####
+add_page_config(st)
+
+#### sidebar ####
+add_sidebar(st)
+
+st.info(
+    "이 사이트는 **국토교통부 실거래가 공개시스템**의 데이터로 만들어졌습니다. ([출처](https://rt.molit.go.kr/pt/xls/xls.do?mobileAt=))"
+)
+
+st.markdown("""
+### 🩺 이 사이트를 만든 배경
+
+아파트 실거래는 찾기 쉬운데, 빌라 실거래는 발견할 수 없어 만들어보았습니다.
+공공데이터를 이용하여 집계된 데이터를 분석하여 인사이트를 얻을 수 있을 것입니다.
+아직 베타 버전이라 많은 기능은 없지만 계속해서 업데이트할 예정입니다. 🔥🔥🔥""")
+
+st.divider()
+st.markdown("""
+### 😘 이런분께 추천해요!
+
+- 빌라 시장에 대한 인사이트가 필요하신 분
+- 아파트는 비싸 부담스럽고 주위에서 빌라는 사면 안된다고 하여 망설이는 분
+- 영끌없이 자산 시장에 진입하고 싶으신 분
+- 빌라 투자에 망설이는 분
+- 빌라 전세 사기 때문에 빌라 전세 살기에 망설이는 분
+""")
+
+st.divider()
+st.markdown("""
+### 🧑‍💻 개발 스토리
+
+- 24시간 만에 (아이와 놀면서) 빌라 실거래가 분석 사이트 구축 ([링크](https://open.substack.com/pub/imissyoubrad/p/24?r=4gagwz&utm_campaign=post&utm_medium=web&showWelcomeOnShare=true))
+""")
--- sidebar.py
+++ sidebar.py
@@ -20,10 +20,6 @@ __footer_style = """
 
 def add_sidebar(st: streamlit):
     st.sidebar.header("✨빌라 실거래 | 빌라 노노✨")
-    st.sidebar.divider()
-    st.sidebar.markdown(
-        "이 사이트는 **국토교통부 실거래가 공개시스템**의 데이터로 만들어졌습니다. ([출처](https://rt.molit.go.kr/pt/xls/xls.do?mobileAt=))"
-    )
 
     # Footer 추가
     st.sidebar.markdown(__footer_style, unsafe_allow_html=True)
